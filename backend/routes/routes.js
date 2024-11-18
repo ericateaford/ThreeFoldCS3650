4eb669ac3c2cb12548d8907f47edee59c5b161cd
Logan Burnett <burnettloganj@gmail.com>
2024-11-17 17:17:27 -0700

    Improved backend error handling

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -16,19 +16,29 @@ router.get("/data", (req, res) => {
 
 router.post("/user/add", async (req, res) => {
   const data = readData();
+  let errors = {};
 
   const newItem = req.body;
   if (!newItem) {
     //checks to ensure blank field is not being saved
     return res.status(400).json({ error: "Entry cannot be empty" }); //bad request error
   } 
-  else if (!newItem.username) {
-    return res.status(400).json({ error: "Entry needs username field" });
-  } 
-  else if (!newItem.password) {
-    return res.status(400).json({ error: "Entry needs password field" });
+  
+  if (!newItem.username) {
+    errors.username = "Entry needs username field";
   }
-  // TODO Should also check if the username is already being used 
+
+  if (!newItem.password) {
+    errors.password = "Entry needs password field";
+  }
+  // TODO Should also check if the username is already being used
+  
+  if (Object.keys(errors).length > 0) {
+    return res.status(422).json({
+      message: "User signup failed due to validation errors.",
+      errors,
+    });
+  } 
 
   const userId = generateId();
   const hashedPass = await hash(newItem.password, 10)
