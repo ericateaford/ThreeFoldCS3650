a8ed4f2d58fa2749861941fc6c318fbc83b4b69f
Madison Christensen <mchristensen49@mail.weber.edu>
2024-11-26 13:42:28 -0700

    Carousel for daily ritual working

--- src/components/DailyRitualCarousel.js
+++ src/components/DailyRitualCarousel.js
@@ -3,14 +3,16 @@ import {Link} from "react-router-dom" //Import link from react-router-dom
 function DailyRitualCarousel() {
     return (
 
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+<div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
   <div> <h3 class="carouselheading text-start">Daily Ritual</h3> </div>
   <div class="carousel-inner">
+
     <div class="carousel-item active">
-      <div class="cards-wrapper">
+     {/* <div class="cards-wrapper">*/}
+
+      <div className="cards-wrapper d-flex justify-content-center flex-wrap">
 
       <Link to="/Journal">
-
       <div class="card mb-5">
       <img src="/images/Planner.png" class="card-img-top" alt="..."/>
         <div class="card-body">
@@ -18,18 +20,27 @@ function DailyRitualCarousel() {
         </div>
       </div>
       </Link>
+      </div>
+      </div>
 
+      <div className="carousel-item">
+      <div className="cards-wrapper d-flex justify-content-center">
+           
       <Link to="Citrine">
-      <div class="card d-none d-md-block">
+      <div class="card mb-5">
       <img src="/images/citrine.png" class="card-img-top" alt="..."/>
         <div class="card-body">
         <div class="carouselTitles">Citrine and It's Benefits</div>
         </div>
       </div>
       </Link>
+      </div>
+      </div>
 
+      <div className="carousel-item">
+          <div className="cards-wrapper d-flex justify-content-center">
       <Link to="Routine">
-      <div class="card d-none d-md-block">
+      <div class="card mb-5">
       <img src="/images/tree.png" class="card-img-top" alt="..."/>
         <div class="card-body">
         <div class="carouselTitles">Your Connection to Mother Nature</div>
@@ -40,14 +51,19 @@ function DailyRitualCarousel() {
     </div>
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+
+  <button
+  className="carousel-control-prev" data-bs-target="#carouselExampleControls" type="button" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only"></span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="visually-hidden"></span>
+
+  </button>
+
+  <button
+  className="carousel-control-next" data-bs-target="#carouselExampleControls" type="button" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only"></span>
-  </a>
+    <span class="visually-hidden"></span>
+  </button>
   <div>
 
   </div>
